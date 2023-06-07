8b705b8df760c1f649bcf9d80439f34dc89d0343
Abdulla <abdullat64@gmail.com>
2023-06-07 16:35:56 +0400

    msg

--- script_subscriptions_page.js
+++ script_subscriptions_page.js
@@ -18,15 +18,38 @@ function processResult(xhttp) {
 getdata();
 
 function showMySubscriptions(){
+    
     var keys = Object.keys(dataObj.Services.Mobile_Services.PostPaid[0]);
 
     for(var i = 0; i < dataObj.Services.Mobile_Services.PostPaid.length; i++){
-        document.getElementById("data").innerHTML += "<p>" + dataObj.Services.Mobile_Services.PostPaid[i].Name + "<\p>" +
+        document.querySelector(".post-paid-plans").innerHTML += "<p>" + dataObj.Services.Mobile_Services.PostPaid[i].Name + "<\p>" +
         keys[1] + ": " + dataObj.Services.Mobile_Services.PostPaid[i].Data + "&emsp;" +
         keys[2] + ": " + dataObj.Services.Mobile_Services.PostPaid[i].Voice + "&emsp;" +
         keys[3] + ": " + dataObj.Services.Mobile_Services.PostPaid[i].Price + "&emsp;" +
         keys[4] + ": " + dataObj.Services.Mobile_Services.PostPaid[i].Time + "</p>" +
-        "<div class=\"unsubscribe__btn--right\"> <button class=\"unsubscribe__btn\" onClick=\"unsubscribe()\">Unsubscribe</button><hr></div>";
+        "<div class=\"unsubscribe__btn\"> <button class=\"unsubscribe__btn--right\" onClick=\"unsubscribe()\">Unsubscribe</button><hr></div>";
+    }
+
+    var keys_home_internet = Object.keys(dataObj.Services.TV_Wireless_Services.Home_Internet[0]);
+
+    for(var i = 0; i < dataObj.Services.TV_Wireless_Services.Home_Internet.length; i++){
+        document.querySelector(".home-internet").innerHTML += "<p>" + dataObj.Services.TV_Wireless_Services.Home_Internet[i].Name + "<\p>" +
+        keys_home_internet[0] + ": " + dataObj.Services.TV_Wireless_Services.Home_Internet[i].Data + "&emsp;" +
+        keys_home_internet[1] + ": " + dataObj.Services.TV_Wireless_Services.Home_Internet[i].Speed + "&emsp;" +
+        keys_home_internet[2] + ": " + dataObj.Services.TV_Wireless_Services.Home_Internet[i].Router + "&emsp;" +
+        keys_home_internet[3] + ": " + dataObj.Services.TV_Wireless_Services.Home_Internet[i].Price + "&emsp;" +
+        keys_home_internet[4] + ": " + dataObj.Services.TV_Wireless_Services.Home_Internet[i].Time + "</p>" +
+        "<div class=\"unsubscribe__btn\"> <button class=\"unsubscribe__btn--right\" onClick=\"unsubscribe()\">Unsubscribe</button><hr></div>";
+    }
+
+    var keys_tv_packages = Object.keys(dataObj.Services.TV_Wireless_Services.TV_Packages[0]);
+
+    for(var i = 0; i < dataObj.Services.TV_Wireless_Services.TV_Packages.length; i++){
+        document.querySelector(".tv-packages").innerHTML += "<p>" + dataObj.Services.TV_Wireless_Services.Home_Internet[i].Name + "<\p>" +
+        keys_tv_packages[1] + ": " + dataObj.Services.TV_Wireless_Services.TV_Packages[i].Channels + "&emsp;" +
+        keys_tv_packages[3] + ": " + dataObj.Services.TV_Wireless_Services.TV_Packages[i].Price + "&emsp;" +
+        keys_tv_packages[4] + ": " + dataObj.Services.TV_Wireless_Services.TV_Packages[i].Time + "</p>" +
+        "<div class=\"unsubscribe__btn\"> <button class=\"unsubscribe__btn--right\" onClick=\"unsubscribe()\">Unsubscribe</button><hr></div>";
     }
     
 }
